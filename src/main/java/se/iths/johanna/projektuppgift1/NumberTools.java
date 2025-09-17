b6a8d65212b54118497f6b748ffd3a500a1b07aa
Johannavii <jh.viippa@gmail.com>
2025-09-17 15:10:30 +0200

    Uppdaterad

--- src/main/java/se/iths/johanna/projektuppgift1/NumberTools.java
+++ src/main/java/se/iths/johanna/projektuppgift1/NumberTools.java
@@ -1,6 +1,16 @@
 package se.iths.johanna.projektuppgift1;
 
 public class NumberTools {
+    // Metod 2: Returnerar maxtalet i en int-array
+    public static int getMaxValue(int[] numbers) {
+        int max = numbers[0];
+        for (int num : numbers) {
+            if (num > max) {
+                max = num;
+            }
+        }
+        return max;
+    }
 
     // Metod 3: Returnerar maxtalet av två double-parametrar med en ternär operator
     public static double getMaxDouble(double a, double b) {
