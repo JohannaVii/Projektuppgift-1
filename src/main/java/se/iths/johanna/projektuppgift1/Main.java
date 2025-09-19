703d4728038e4d221b69f96397aeeabdba0dac2c
Johannavii <jh.viippa@gmail.com>
2025-09-19 09:46:33 +0200

    Uppdaterad - Static har tagits bort, Ett radbyte har lagts till

--- src/main/java/se/iths/johanna/projektuppgift1/Main.java
+++ src/main/java/se/iths/johanna/projektuppgift1/Main.java
@@ -5,21 +5,26 @@ public class Main {
     // Main-metod - Här anropas alla metoder
     public static void main(String[] args) {
         System.out.println("\nPROJEKTUPPGIFT 1: Metoder och arrayer\n");
+
+        //UPPDATERING - Skapar objekt av klasserna
+        TextTools textTools = new TextTools();
+        NumberTools numberTools = new NumberTools();
+
         System.out.println("Metod 1 - String-array\n");
         String[] text = {"Hello", "World", "!"};
         System.out.println("Utskriven array: ");
-        TextTools.printArray(text);
-        System.out.println("\n\n---\n");
+        textTools.printArray(text);
+        System.out.println("\n---\n");
 
         System.out.println("Metod 2 - Int-array\n");
-        int[] values = {8, 88, 49, 54, 85};
-        int maxValue = NumberTools.getMaxValue(values);
+        int[] values = {-8, 88, -49, 54, 85};
+        int maxValue = numberTools.getMaxValue(values);
         System.out.println("Största heltalet i arrayen: ");
         System.out.println(maxValue);
         System.out.println("\n---\n");
 
         System.out.println("Metod 3 - Två double-parametrar\n");
-        double result = NumberTools.getMaxDouble(3.14, 0.31);
+        double result = numberTools.getMaxDouble(3.14, 0.31);
         System.out.println("Största talet av 3.14 och 0.31: ");
         System.out.println(result + " aka π");
         System.out.println("\n---\n");
@@ -27,7 +32,7 @@ public class Main {
         System.out.println("Metod 4 - Två String-parametrar\n");
         //System.out.println("Full name: ");
         System.out.println("My name is Bond, ");
-        System.out.println(TextTools.printFullName("James", "Bond"));
+        System.out.println(textTools.printFullName("James", "Bond"));
         System.out.println("\n---\n");
     }
 }
--- src/main/java/se/iths/johanna/projektuppgift1/TextTools.java
+++ src/main/java/se/iths/johanna/projektuppgift1/TextTools.java
@@ -3,14 +3,16 @@ package se.iths.johanna.projektuppgift1;
 public class TextTools {
 
     // Metod 1: Skriver ut alla ord i en String-array med mellanslag
-    public static void printArray(String[] textArray) {
+    public void printArray(String[] textArray) {
         for (String text : textArray) {
             System.out.print(text + " ");
         }
+        //UPPDATERING - Hoppar till nästa rad
+        System.out.println();
     }
 
     // Metod 4: Skriver ut två String-parametrar med ett mellanslag
-    public static String printFullName(String firstName, String lastName) {
+    public String printFullName(String firstName, String lastName) {
         return firstName + " " + lastName;
     }
 }
